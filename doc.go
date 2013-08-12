f2a78e8827a402a7893e53196af4440779edc1d0
Simon Zimmermann <simon@insmo.com>
2013-08-12 11:46:13 +0200

    add go doc

---
+++ doc.go
@@ -0,0 +1,77 @@
+// Copyright (c) 2013 Simon Zimmermann
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// package profanity is a HTTP server which implements a
+// simple API.  It exposes the `filter.Update()`,
+// `filter.Replace()`, `filter.Remove()` and
+// `filter.Sanitize()` methods. 
+// 
+// API
+// 
+// Create/overwrite blacklist.
+// 
+//     POST --data "blacklist=x&blacklist=xx&blacklist=xxx" /api/1.0/blacklist/
+// 
+//     HTTP/1.1 201 Created
+//     Date: Mon, 12 Aug 2013 09:37:17 GMT
+//     Content-Length: 0
+//     Content-Type: text/plain; charset=utf-8
+// 
+// Return current blacklist.
+// 
+//     GET /api/1.0/blacklist/
+// 
+//     HTTP/1.1 200 OK
+//     Content-Type: application/json; charset=utf-8
+//     Date: Mon, 12 Aug 2013 09:34:44 GMT
+//     Transfer-Encoding: chunked
+// 
+//     ["x", "xx", "xxx"]    
+// 
+// Update blacklist.
+// 
+//     PUT --data "blacklist=y" /api/1.0/blacklist/
+// 
+//     HTTP/1.1 200 OK
+//     Date: Mon, 12 Aug 2013 09:37:17 GMT
+//     Content-Length: 0
+//     Content-Type: text/plain; charset=utf-8
+// 
+// Remove from blacklist.
+// 
+//     PUT --data "blacklist=y" /api/1.0/blacklist/remove/
+// 
+//     HTTP/1.1 200 OK
+//     Date: Mon, 12 Aug 2013 09:37:17 GMT
+//     Content-Length: 0
+//     Content-Type: text/plain; charset=utf-8
+// 
+// Sanitize text.
+// 
+//     GET /api/1.0/sanitize/?text=foo%20bar%20xxx
+// 
+//     HTTP/1.1 200 OK
+//     Date: Mon, 12 Aug 2013 09:37:38 GMT
+//     Content-Type: application/json; charset=utf-8
+//     Content-Length: 33
+// 
+//     {"text":"foo bar ***","lang":""}
+package main
