5d3777a343d4b8ad5b970aa858f12ebe16530dbd
Simon Zimmermann <simon@insmo.com>
2015-10-29 13:35:35 +0100

    godeps: update

--- Godeps/_workspace/src/github.com/garyburd/redigo/internal/redistest/testdb.go
+++ Godeps/_workspace/src/github.com/garyburd/redigo/internal/redistest/testdb.go
@@ -1,4 +1,4 @@
-// Copyright 2012 Gary Burd
+// Copyright 2014 Gary Burd
 //
 // Licensed under the Apache License, Version 2.0 (the "License"): you may
 // not use this file except in compliance with the License. You may obtain
@@ -12,17 +12,18 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-package redis
+// Package redistest contains utilities for writing Redigo tests.
+package redistest
 
 import (
-	"bufio"
 	"errors"
-	"net"
 	"time"
+
+	"github.com/garyburd/redigo/redis"
 )
 
 type testConn struct {
-	Conn
+	redis.Conn
 }
 
 func (t testConn) Close() error {
@@ -37,41 +38,31 @@ func (t testConn) Close() error {
 	return t.Conn.Close()
 }
 
-// DialTestDB dials the local Redis server and selects database 9. To prevent
+// Dial dials the local Redis server and selects database 9. To prevent
 // stomping on real data, DialTestDB fails if database 9 contains data. The
 // returned connection flushes database 9 on close.
-func DialTestDB() (Conn, error) {
-	c, err := DialTimeout("tcp", ":6379", 0, 1*time.Second, 1*time.Second)
+func Dial() (redis.Conn, error) {
+	c, err := redis.DialTimeout("tcp", ":6379", 0, 1*time.Second, 1*time.Second)
 	if err != nil {
 		return nil, err
 	}
 
 	_, err = c.Do("SELECT", "9")
 	if err != nil {
+		c.Close()
 		return nil, err
 	}
 
-	n, err := Int(c.Do("DBSIZE"))
+	n, err := redis.Int(c.Do("DBSIZE"))
 	if err != nil {
+		c.Close()
 		return nil, err
 	}
 
 	if n != 0 {
+		c.Close()
 		return nil, errors.New("database #9 is not empty, test can not continue")
 	}
 
 	return testConn{c}, nil
 }
-
-type dummyClose struct{ net.Conn }
-
-func (dummyClose) Close() error { return nil }
-
-// NewConnBufio is a hook for tests.
-func NewConnBufio(rw bufio.ReadWriter) Conn {
-	return &conn{br: rw.Reader, bw: rw.Writer, conn: dummyClose{}}
-}
-
-var (
-	ErrNegativeInt = errNegativeInt
-)
